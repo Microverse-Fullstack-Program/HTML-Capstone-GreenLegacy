25b13fd1936cd6b1d2d2473e16ebc0df7d95795e
cherelemma <73167960+cherelemma@users.noreply.github.com>
2022-12-03 18:15:13 +0300

    Incorporate reviewer's optional suggestion

--- assests/JavaScript/main.js
+++ assests/JavaScript/main.js
@@ -20,47 +20,42 @@ const eventData = [
     img: 'assests/Images/ethiopian-pm.png',
     eventName: 'Ethiopian PM',
     eventTitle: 'PM during 2022 Green Legacy Campaing Launch',
-    desc: `The Prime Minister has officially launched the fourth green legacy program which will be 
-            implemented under the theme “Our Imprint for Our Children”.`,
+    desc: `The Prime Minister has officially launched the fourth green legacy program.`,
   },
   {
     img: 'assests/Images/oromia-planting.png',
     eventName: 'Oromia State',
     eventTitle: 'Oromia State Launches A Campaign To Plant 400 Million Trees In A Day',
-    desc: `Oromia State Chief Administrator Shimelis Abdisa has officially launched 
-          the annual Green Legacy Initiative in the presence of high level government officials
-            and public representatives at regional level. `,
+    desc: `Oromia State Chief Administrator Shimelis has officially launched 
+          the annual Green Legacy Initiative. `,
   },
   {
     img: 'assests/Images/sudan-duty-pm.png',
-    eventName: 'S. Sudan’s Deputy Foreign Minister',
-    eventTitle: 'Ethiopia-South Sudan Green Legacy 2022',
-    desc: `Deputy Minister for Foreign Affairs and International Cooperation of the Republic of South Sudan, 
-          Deng Dau Deng thanked the Ethiopian Community and members of the peacekeeping force for their participation
-          in the tree planting program.`,
+    eventName: 'Deputy Foreign Minister',
+    eventTitle: 'Ethio-South Sudan Green Legacy 2022',
+    desc: `Deputy Minister for Foreign Affairs the Republic of South Sudan, 
+          Deng Dau Deng thanked the Ethiopian Community.`,
   },
   {
     img: 'assests/Images/moe-universities.png',
-    eventName: 'Public Universities Officials',
-    eventTitle: 'Ethiopian public Universities launch the 2022 Green Legacy Initiative',
-    desc: `Ethiopian Ministry of Education (MoE), in collaboration with a coalition of four public universities
-           in Addis Ababa and the attendance of top officials of the Ministry and leaders of the universities, 
-           have officially launched the 2021 Green Legacy Initiative at Addis Ababa `,
+    eventName: 'Public Universities',
+    eventTitle: 'Public Universities launch the 2022 GLI',
+    desc: `Ethiopian Ministry of Education (MoE), in collaboration with a coalition of public universities
+           in officially launched the 2021 GLI `,
   },
   {
     img: 'assests/Images/total-eng.png',
     eventName: 'TotalEnergies Ethiopia',
-    eventTitle: 'Total Ethiopia Took Part in the National Green Legacy Campaign',
+    eventTitle: 'Total Ethiopia Took Part in GLI',
     desc: `Total Ethiopia took part in the National Green Legacy campaign on July 29, 2019. 
           The objective of the campaign was to plant more than 200 million tree saplings in one day.`,
   },
   {
     img: 'assests/Images/selfhelp-africa.png',
     eventName: 'Self Help Africa',
-    eventTitle: 'Self Help Africa Group Played a part in the Green legacy initiative',
+    eventTitle: 'SelfHelp Africa Played part in GLI',
     desc: `Self Help Africa is working with communities, as part of Ethiopia’s green legacy, 
-          to plant a total of 300,000 tree seedlings on degraded farmlands around Lake Ziway, 
-          close to Adami Tulu, the site of the organisation’s very first African development project.`,
+          to plant a total of 300,000 tree seedlings on degraded farmlands.`,
   },
 ];
 
